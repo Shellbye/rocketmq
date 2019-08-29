9e5af384bd8daca970bc2fcce30a8a596082a05f
baijianguo <baijianguo@100tal.com>
2019-08-29 20:08:21 +0800

    重构版本成功，循环不如循环展开快

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -2,88 +2,32 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
-    public String nearestPalindromic(String n) {
-        // 个位数需要单独处理
-        if (n.length() == 1) {
-            return String.valueOf(Long.parseLong(n) - 1);
-        }
-        // 10, 11 单独处理
-        if ("10".equals(n) || "11".equals(n)) {
-            return "9";
-        }
-        if (n.length() % 2 == 0) {  // 长度为偶数，可能是 123321 之类（称为轴对称）
-//            return copyEvenHigh2Low(n, (long) Math.pow(10, n.length() - 1) - 1, (long) Math.pow(10, n.length()) + 1);
-            return copyHigh2Low(n, (long) Math.pow(10, n.length() - 1) - 1, (long) Math.pow(10, n.length()) + 1);
-        } else {    // 长度为奇数，可能是 12321 之类（称为中心对称）
-            return copyOddHigh2Low(n, (long) Math.pow(10, n.length() - 1) - 1, (long) Math.pow(10, n.length()) + 1);
-//            return copyHigh2Low(n, (long) Math.pow(10, n.length() - 1) - 1, (long) Math.pow(10, n.length()) + 1);
-        }
-    }
-
     /**
      * 核心在于寻找比 n 大和比 n 小的两个回文数
-     * 直接高位反转：结果可能大也可能小，取决于高位反转之后与原来低位的大小   = a
-     *
-     * 比 n 大：
-     *      高位+1反转                                               = b
-     *      低位上升
-     * 比 n 小：
-     *      高位-1反转                                               = c
-     *      低位下降
-     *
-     *
-     * 99999 和 10000 这种有个比较特殊的地方，就是他们的高位在加一减一之后，会发生位数的变化
-     *
+     * 高位+1反转
+     * 高位直接反转
+     * 高位-1反转
      * "那么实际上最近回文数是有范围的，比如说n为三位数，那么其最近回文数的范围在[99, 1001]之间，
      * 这样我们就可以根据给定数字的位数来确定出两个边界值，要和其他生成的回文数进行比较，取绝对差最小的。"
      * ——https://www.cnblogs.com/grandyang/p/6915355.html
-     * */
-    private long getReversed(long value, boolean even) {
-        if (even) {
-            return Long.parseLong(new StringBuffer(String.valueOf(value)).reverse().toString());
-        } else {
-            if (value > 10) {
-                return Long.parseLong(new StringBuffer(String.valueOf(value / 10)).reverse().toString());
-            } else {
-                return Long.parseLong(new StringBuffer(String.valueOf(value)).reverse().toString());
-            }
-        }
-    }
-
-    private String copyOddHigh2Low(String n, long min, long max) {
-        // 奇数时，高位要把中间值拉上，因为中间值减一可能会涉及到向高位借位（比如 902）
-        int half = n.length() / 2 + 1;
-        // 原始高位和它放到低位之后的值
-        long highVal = Long.parseLong(n.substring(0, half));
-        // 这里的除10是为了去掉多取的那个中间位，因为反转不需要中间位，否则会多出来一位
-        String highValReversedStr = new StringBuffer(String.valueOf(highVal / 10)).reverse().toString();
-        long h = Long.parseLong(highVal + highValReversedStr);
-        // 高位减一和它放到低位之后的值
-        long highValM1 = highVal - 1;   // 这里减一之后，可能会少一位
-        String highValM1ReversedStr = new StringBuffer(String.valueOf(highValM1 / 10)).reverse().toString();
-        long hM = Long.parseLong(highValM1 + highValM1ReversedStr);
-        if (hM < min)
-            hM = min;
-        long highValA1 = highVal + 1;   // 这里加一之后，也可能会多一位
-        String highValA1ReversedStr = new StringBuffer(String.valueOf(highValA1 / 10)).reverse().toString();
-        long hA = Long.parseLong(highValA1 + highValA1ReversedStr);
-        if (hA > max)
-            hA = max;
-        return judgeAndReturn(h, hM, hA, n);
-    }
-
-    private String copyHigh2Low(String n, long min, long max) {
-        int half = n.length() / 2;
+     */
+    public String nearestPalindromic(String n) {
+        int half = n.length() / 2 + n.length() % 2; // 统一奇偶
         long value = Long.parseLong(n);
         long highVal0 = Long.parseLong(n.substring(0, half));
         Set<Long> candidates = new HashSet<>();
-        candidates.add(max);
-        candidates.add(min);
+        candidates.add((long) Math.pow(10, n.length() - 1) - 1);
+        candidates.add((long) Math.pow(10, n.length()) + 1);
         for (int i = -1; i <= 1; i++) {
             String pre = String.valueOf(highVal0 + i);
-            String middle = n.length() % 2 == 0 ? "" : String.valueOf(n.charAt(half + 1));
-            String post = new StringBuffer(pre).reverse().toString();
-            Long candidate = Long.parseLong(String.format("%s%s%s", pre, middle, post));
+            String post = "";
+            if (n.length() % 2 == 0) { // 偶数，前半部分整体反转
+                post = new StringBuffer(pre).reverse().toString();
+            } else {                   // 奇数，需要注意中间不反转，减少一位即可
+                post = new StringBuffer(pre.substring(0, pre.length() - 1)).reverse().toString();
+            }
+
+            Long candidate = Long.parseLong(String.format("%s%s", pre, post));
             candidates.add(candidate);
         }
         candidates.remove(value);
@@ -98,49 +42,6 @@ public class Solution {
             }
         }
         return String.valueOf(result);
-    }
-
-    private String copyEvenHigh2Low(String n, long min, long max) {
-        /*
-          单纯的把高位拷贝到低位是不行的，因为无法确保符合 最近的回文数 中的 最近（比如 9723）
-          这里的 最近，需要计算一高一低两个值，然后计算与输入与这两个值的绝对值，取较小的
-        */
-        int half = n.length() / 2;
-        // 高位反转
-        long highVal = Long.parseLong(n.substring(0, half));
-        String highValReversedStr = new StringBuffer(String.valueOf(highVal)).reverse().toString();
-        long h = Long.parseLong(highVal + highValReversedStr);
-
-        // 高位减一
-        long highValM1 = highVal - 1;
-        String highValM1ReversedStr = new StringBuffer(String.valueOf(highValM1)).reverse().toString();
-        long hM = Long.parseLong(highValM1 + highValM1ReversedStr);
-        if (hM < min)
-            hM = min;
-        // 高位加一
-        long highValA1 = highVal + 1;
-        String highValA1ReversedStr = new StringBuffer(String.valueOf(highValA1)).reverse().toString();
-        long hA = Long.parseLong(highValA1 + highValA1ReversedStr);
-        if (hA > max)
-            hA = max;
-        return judgeAndReturn(h, hM, hA, n);
-    }
-
-    private String judgeAndReturn(long h, long hM, long hA, String n) {
-        long val = Long.parseLong(n);
-        long d1 = Math.abs(val - h);
-        long d2 = Math.abs(val - hM);
-        long d3 = Math.abs(val - hA);
-        if (d1 == 0) {  // 本身就是回文数
-            d1 = Long.MAX_VALUE;
-        }
-        if (d2 <= d1 && d2 <= d3) {
-            return String.valueOf(hM);  // 最小值
-        }
-        if (d1 <= d2 && d1 <= d3) {
-            return String.valueOf(h);
-        }
-        return String.valueOf(hA);
     }
 
     public static void main(String[] args) {
