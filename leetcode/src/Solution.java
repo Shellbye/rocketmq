f0723275b830e5f12984aa75eb55f9086724d7b7
baijianguo <baijianguo@100tal.com>
2019-09-23 17:41:48 +0800

    螺旋矩阵

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -2,100 +2,72 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
-    /**
-     * 核心在于寻找比 n 大和比 n 小的两个回文数
-     * 高位+1反转
-     * 高位直接反转
-     * 高位-1反转
-     * "那么实际上最近回文数是有范围的，比如说n为三位数，那么其最近回文数的范围在[99, 1001]之间，
-     * 这样我们就可以根据给定数字的位数来确定出两个边界值，要和其他生成的回文数进行比较，取绝对差最小的。"
-     * ——https://www.cnblogs.com/grandyang/p/6915355.html
-     */
-    public String nearestPalindromic(String n) {
-        int half = n.length() / 2 + n.length() % 2; // 统一奇偶
-        long value = Long.parseLong(n);
-        long highVal0 = Long.parseLong(n.substring(0, half));
-        Set<Long> candidates = new HashSet<>();
-        candidates.add((long) Math.pow(10, n.length() - 1) - 1);
-        candidates.add((long) Math.pow(10, n.length()) + 1);
-        for (int i = -1; i <= 1; i++) {
-            String pre = String.valueOf(highVal0 + i);
-            String post = "";
-            if (n.length() % 2 == 0) { // 偶数，前半部分整体反转
-                post = new StringBuffer(pre).reverse().toString();
-            } else {                   // 奇数，需要注意中间不反转，减少一位即可
-                post = new StringBuffer(pre.substring(0, pre.length() - 1)).reverse().toString();
+
+    public int[][] generateMatrix(int n) {
+        int[][] ret = new int[n][n];
+        int x = 0;
+        int y = 0;
+        String direction = ">";
+        int rightBorder = n;
+        int bottomBorder = n;
+        int leftBorder = 0;
+        int topBorder = 1;
+        for(int i = 1; i <= n*n; i++) {
+            // 写入当前值
+            ret[y][x] = i;
+            // 计算接下来的方向
+            if (x + 1 == rightBorder && direction.equals(">")) {   // 到达了右边界，转头向下，边界也在不断变化，每次到达之后都需要修改
+                rightBorder--;
+                y++;
+                direction = "v";
+            } else if (y + 1 == bottomBorder && direction.equals("v")) {    // 到达了下边界
+                bottomBorder--;
+                x--;
+                direction = "<";
+            } else if (x == leftBorder && direction.equals("<")) {
+                leftBorder++;
+                y--;
+                direction = "^";
+            } else if (y == topBorder && direction.equals("^")) {
+                topBorder++;
+                x++;
+                direction = ">";
+            } else {    // 没有到达任何边界，继续
+                // 计算接下来的坐标值
+                switch (direction) {
+                    case ">":
+                        x++;
+                        break;
+                    case "v":
+                        y++;
+                        break;
+                    case "<":
+                        x--;
+                        break;
+                    case "^":
+                        y--;
+                        break;
+                    default:
+                        break;
+                }
             }
 
-            Long candidate = Long.parseLong(String.format("%s%s", pre, post));
-            candidates.add(candidate);
         }
-        candidates.remove(value);
-        long result = value;
-        long minDif = Long.MAX_VALUE;
-        for (Long a : candidates) {
-            if (Math.abs(a - value) < minDif) {
-                minDif = Math.abs(a - value);
-                result = a;
-            } else if (Math.abs(a - value) == minDif) {
-                result = Math.min(result, a);
-            }
-        }
-        return String.valueOf(result);
+        return ret;
     }
+
 
     public static void main(String[] args) {
-        p(new Solution().nearestPalindromic("1"), "0");
-        p(new Solution().nearestPalindromic("9"), "8");
-        p(new Solution().nearestPalindromic("10"), "9");
-        p(new Solution().nearestPalindromic("11"), "9");
-        p(new Solution().nearestPalindromic("12"), "11");
-        p(new Solution().nearestPalindromic("13"), "11");
-        p(new Solution().nearestPalindromic("14"), "11");
-        p(new Solution().nearestPalindromic("15"), "11");
-        p(new Solution().nearestPalindromic("16"), "11");
-        p(new Solution().nearestPalindromic("16"), "11");
-        p(new Solution().nearestPalindromic("17"), "22");
-        p(new Solution().nearestPalindromic("111"), "101");
-        p(new Solution().nearestPalindromic("123"), "121");
-        p(new Solution().nearestPalindromic("100"), "99");
-        p(new Solution().nearestPalindromic("199"), "202");
-        p(new Solution().nearestPalindromic("902"), "898");
-        p(new Solution().nearestPalindromic("999"), "1001");//
-        p(new Solution().nearestPalindromic("1111"), "1001");
-        p(new Solution().nearestPalindromic("1000"), "999");//
-        p(new Solution().nearestPalindromic("1010"), "1001");
-        p(new Solution().nearestPalindromic("1112"), "1111");
-        p(new Solution().nearestPalindromic("1234"), "1221");
-        p(new Solution().nearestPalindromic("1999"), "2002");
-        p(new Solution().nearestPalindromic("90100"), "90109");
-        p(new Solution().nearestPalindromic("9001000"), "9001009");
-        p(new Solution().nearestPalindromic("900000"), "899998");
-        p(new Solution().nearestPalindromic("1805170081"), "1805115081");
-        p(new Solution().nearestPalindromic("9723"), "9669");   //  9724 = (9779 + 9669) / 2
-        p(new Solution().nearestPalindromic("9724"), "9669");   //  9724 = (9779 + 9669) / 2
-        p(new Solution().nearestPalindromic("9725"), "9779");   //  9724 = (9779 + 9669) / 2
-        p(new Solution().nearestPalindromic("9900"), "9889");   //
-        p(new Solution().nearestPalindromic("9938"), "9889");   //
-        p(new Solution().nearestPalindromic("9944"), "9889");   //  9944 = (9999 + 9889) / 2
-        p(new Solution().nearestPalindromic("9945"), "9999");
-        p(new Solution().nearestPalindromic("9977"), "9999");   //
-        p(new Solution().nearestPalindromic("12321"), "12221");
-        p(new Solution().nearestPalindromic("123321"), "122221");
-        p(new Solution().nearestPalindromic("12932"), "12921");
-        p(new Solution().nearestPalindromic("99800"), "99799");
-        p(new Solution().nearestPalindromic("12120"), "12121");
-        p(new Solution().nearestPalindromic("807045053224792883"), "807045053350540708");
-
+        int[][] r0 = new Solution().generateMatrix(5);
+        p(r0);
     }
 
-    public static void p(Object input, String answer) {
-//        System.out.println(input);
-        if (!answer.equals(input)) {
-            System.err.println(input + ", but expect " + answer);
-            System.exit(0);
-        } else {
-            System.out.println("success as " + answer);
+    public static void p(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.println("");
         }
     }
 
