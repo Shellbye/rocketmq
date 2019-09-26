174deb004a2b47182cff393783ca9361eb7e2891
baijianguo <baijianguo@100tal.com>
2019-09-26 14:32:24 +0800

    最长公共前缀

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -4,24 +4,27 @@ import java.util.List;
 public class Solution {
 
 
-    public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
+    public String longestCommonPrefix(String[] strs) {
+        String s = "";
+        if (strs.length < 1) {
+            return s;
         }
-        int y = 0;
-        int x0 = x;
-        while (x / 10 > 0) {
-            y = y * 10 + x % 10;
-            x = x / 10;
+        for (int i = 0; i <= strs[0].length(); i++) {
+            s = strs[0].substring(0, i);
+            for (String s1 : strs) {
+                if (!s1.startsWith(s)) {
+                    return s.substring(0, s.length() - 1);
+                }
+            }
         }
-        y = y * 10 + x % 10;
-        return x0 == y;
+        return s;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().isPalindrome(1234321));
-        System.out.println(new Solution().isPalindrome(123321));
-        System.out.println(new Solution().isPalindrome(1233211));
+        String[] strings = new String[]{"flower","flow","flight"};
+//        String[] strings = new String[]{"1flower","2flow","flight"};
+//        String[] strings = new String[]{"a"};
+        System.out.println(new Solution().longestCommonPrefix(strings));
     }
 
     public static void p(List<List<Integer>> lists) {
