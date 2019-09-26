ce5ef4fbf4a861e66924bd1545b2f00136ad3ca5
baijianguo <baijianguo@100tal.com>
2019-09-26 16:16:05 +0800

    合并两个有序列表

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -3,46 +3,54 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Stack;
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode(int x) {
+        val = x;
+    }
+}
 public class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> chars = new Stack<>();
-        for (Character c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                chars.push(c);
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode m = head;
+        while (l1 != null && l2 != null) {
+            int v = 0;
+            if (l1.val < l2.val) {
+                v = l1.val;
+                l1 = l1.next;
             } else {
-                if (chars.isEmpty()) {
-                    return false;
-                }
-                Character cOut = chars.pop();
-                switch (cOut) {
-                    case '(':
-                        if (c != ')') {
-                            return false;
-                        }
-                        break;
-                    case '[':
-                        if (c != ']') {
-                            return false;
-                        }
-                        break;
-                    case '{':
-                        if (c != '}') {
-                            return false;
-                        }
-                        break;
-                    default:
-                        return false;
-                }
+                v = l2.val;
+                l2 = l2.next;
             }
-//            System.out.println(c);
+            ListNode tmp = new ListNode(v);
+            m.next = tmp;
+            m = m.next;
         }
-        return chars.isEmpty();
+        if (l1 != null)
+            m.next = l1;
+        if (l2 != null)
+            m.next = l2;
+        return head.next;
     }
 
     public static void main(String[] args) {
-        String strings = "[";
-        System.out.println(new Solution().isValid(strings));
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        ListNode l7 = new ListNode(7);
+        ListNode l8 = new ListNode(8);
+        l1.next = l3;l3.next = l5;l5.next = l7;
+        l2.next = l4;l4.next = l6;l6.next = l8;
+        ListNode r = new Solution().mergeTwoLists(l1, null);
+        while (r != null) {
+            System.out.println(r.val);
+            r = r.next;
+        }
     }
 
     public static void p(List<List<Integer>> lists) {
