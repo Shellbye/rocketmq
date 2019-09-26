b4fcaaf4802e2e60dfc033fe0059453e186c1b2d
baijianguo <baijianguo@100tal.com>
2019-09-26 18:13:47 +0800

    爬楼梯

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -13,27 +13,20 @@ class ListNode {
     }
 }
 public class Solution {
-    public int removeDuplicates(int[] nums) {
-        if (nums.length < 1) {
-            return 0;
+    public int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+        int[] cache = new int[n + 1];
+        cache[1] = 1;
+        cache[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            cache[i] = cache[i - 1] + cache[i - 2];
         }
-        int dupCnt = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[dupCnt] != nums[i]) {
-                dupCnt++;
-                nums[dupCnt] = nums[i];
-            }
-        }
-        return dupCnt + 1;
+        return cache[n];
     }
 
     public static void main(String[] args) {
-//        int[] nums = new int[]{1,2,3,3,4,5,6,7,7,8};
-        int[] nums = new int[]{1,1,8};
-//        int[] nums = new int[]{};
-        System.out.println(Arrays.toString(nums));
-        int r = new Solution().removeDuplicates(nums);
-        System.out.println(Arrays.toString(nums));
+        System.out.println(new Solution().climbStairs(18));
     }
 
     public static void p(List<List<Integer>> lists) {
