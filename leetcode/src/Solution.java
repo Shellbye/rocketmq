b88995f132de1f2829ae1b366e2108393527e628
baijianguo <baijianguo@100tal.com>
2019-09-27 12:44:37 +0800

    104. 二叉树的最大深度

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -14,41 +14,34 @@ class ListNode {
     }
 }
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
 public class Solution {
-    // 88. 合并两个有序数组
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // 从后往前合并，可以避免多次移动
-        int pm = m - 1;
-        int pn = n - 1;
-        for (int i = m + n - 1; i >= 0; i--) {
-            if (pm >= 0 && pn >= 0) {
-                if (nums1[pm] > nums2[pn]) {
-                    nums1[i] = nums1[pm];
-                    pm--;
-                } else {
-                    nums1[i] = nums2[pn];
-                    pn--;
-                }
-            } else {
-                if (pm < 0) {
-                    nums1[i] = nums2[pn];
-                    pn--;
-                } else {
-                    nums1[i] = nums1[pm];
-                    pm--;
-                }
-
-            }
-
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
         }
+        int mL = 0;
+        int mR = 0;
+        if (root.left != null) {
+            mL = maxDepth(root.left);
+        }
+        if (root.right != null) {
+            mR = maxDepth(root.right);
+        }
+        return Math.max(mL, mR) + 1;
     }
 
     public static void main(String[] args) {
-        int[] n1 = new int[]{0};
-        int[] n2 = new int[]{};
-        System.out.println(Arrays.toString(n1));
-        new Solution().merge(n1, 0, n2, 0);
-        System.out.println(Arrays.toString(n1));
+        new Solution().maxDepth(null);
     }
 
     public static void p(List<List<Integer>> lists) {
