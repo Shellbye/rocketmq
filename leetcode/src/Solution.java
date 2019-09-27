2c2af2d8bba5ade2f3a1a55196547c6fa57ef0fe
baijianguo <baijianguo@100tal.com>
2019-09-27 12:37:24 +0800

    88. 合并两个有序数组

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Stack;
+
 class ListNode {
     int val;
     ListNode next;
@@ -12,21 +13,42 @@ class ListNode {
         val = x;
     }
 }
+
 public class Solution {
-    public int climbStairs(int n) {
-        if (n <= 2)
-            return n;
-        int[] cache = new int[n + 1];
-        cache[1] = 1;
-        cache[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            cache[i] = cache[i - 1] + cache[i - 2];
+    // 88. 合并两个有序数组
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // 从后往前合并，可以避免多次移动
+        int pm = m - 1;
+        int pn = n - 1;
+        for (int i = m + n - 1; i >= 0; i--) {
+            if (pm >= 0 && pn >= 0) {
+                if (nums1[pm] > nums2[pn]) {
+                    nums1[i] = nums1[pm];
+                    pm--;
+                } else {
+                    nums1[i] = nums2[pn];
+                    pn--;
+                }
+            } else {
+                if (pm < 0) {
+                    nums1[i] = nums2[pn];
+                    pn--;
+                } else {
+                    nums1[i] = nums1[pm];
+                    pm--;
+                }
+
+            }
+
         }
-        return cache[n];
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().climbStairs(18));
+        int[] n1 = new int[]{0};
+        int[] n2 = new int[]{};
+        System.out.println(Arrays.toString(n1));
+        new Solution().merge(n1, 0, n2, 0);
+        System.out.println(Arrays.toString(n1));
     }
 
     public static void p(List<List<Integer>> lists) {
