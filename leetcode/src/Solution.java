5b7b45d6c16fb6d69cd958f1d8929b586458acc8
baijianguo <baijianguo@100tal.com>
2019-09-28 10:17:35 +0800

    环形链表

--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -25,18 +25,38 @@ class TreeNode {
 }
 
 public class Solution {
-    public int singleNumber(int[] nums) {
-        int n = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            n = n ^ nums[i];
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
         }
-        return n;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
 //        int[] a = new int[]{7,1,5,3,6,4};//7    122. 买卖股票的最佳时机 II
-        int[] a = new int[]{1,7,3,1,3,7,6,5,5,2,2};
-        System.out.println(new Solution().singleNumber(a));
+        int[] a = new int[]{1, 7, 3, 1, 3, 7, 6, 5, 5, 2, 2};
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+//        l6.next = l4;
+        System.out.println(new Solution().hasCycle(l1));
     }
 
     public static void p(List<List<Integer>> lists) {
